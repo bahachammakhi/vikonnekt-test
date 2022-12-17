68e8ed62962819b12e116990392b631f8087aec4
Baha chammakhi <bahachammakhi25@gmail.com>
2022-12-17 18:29:06 +0100

    feat: add needed envs

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,5 +1,6 @@
 const envs = {
-  API_URL: process.env.API_URL,
+  API_URL: process.env.API_URL || "https://api.nasa.gov/neo/rest/v1/neo",
+  API_KEY: process.env.API_KEY || "DEMO_KEY",
 };
 
 export default envs;
