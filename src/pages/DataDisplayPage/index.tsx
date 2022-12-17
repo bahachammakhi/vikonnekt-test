4bdffd743085726c3e6e5b3637e4c3b45f384f87
Baha chammakhi <bahachammakhi25@gmail.com>
2022-12-17 19:56:55 +0100

    feat: filter state

--- src/pages/DataDisplayPage/index.tsx
+++ src/pages/DataDisplayPage/index.tsx
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { neoApi } from "../../api";
 import BarChart from "../../components/charts/BarChart";
 import { transformEstimatedDiameters } from "../../components/modules/DataDisplay/transformers";
+import Select from "../../components/ui/Select";
 import useQuery from "../../hooks/useQuery";
 
 export default function DataDisplayPage() {
+  const [filters, setFilters] = useState({
+    orbiting_body: "none",
+  });
   const { data, error, isLoading } = useQuery({
     queryFn: neoApi.getBrowseNeoApi,
     queryKey: "getBrowseNeoApi",
-    transformFn: transformEstimatedDiameters,
+    transformFn: transformEstimatedDiameters(filters),
   });
+
+  function handleChangeFilters(e: any) {
+    setFilters(() => ({
+      ...filters,
+      [e.target.name]: e.target.value,
+    }));
+  }
   if (isLoading) return <div>Loading</div>;
   if (error) return <div>{error}</div>;
   return (
     <div>
-      {data ? (
+      <Select
+        onChange={handleChangeFilters}
+        name="orbiting_body"
+        defaultValue="none"
+        options={[
+          { name: "Earth", value: "Earth" },
+          { name: "Juptr", value: "Juptr" },
+          { name: "Mars", value: "Mars" },
+          { name: "Merc", value: "Merc" },
+          { name: "none", value: "none" },
+        ]}
+      />
+      {data && (data as (string | number)[][]).length > 0 ? (
         <BarChart
           titles={[
             "NEO Name",
