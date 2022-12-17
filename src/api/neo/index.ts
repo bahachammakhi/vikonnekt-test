823a249f611a288dd238aad02c28730995183497
Baha chammakhi <bahachammakhi25@gmail.com>
2022-12-17 19:07:42 +0100

    feat: use data with barchart

--- src/api/neo/index.ts
+++ src/api/neo/index.ts
@@ -1,6 +1,9 @@
 import envs from "../../config/envs";
 import apiCall from "../../utils/fetch";
+import { IGetBrowseNeoApiResponse } from "./types";
 
 export function getBrowseNeoApi() {
-  return apiCall.get(`${envs.API_URL}/browse?api_key=${envs.API_KEY}`);
+  return apiCall.get<IGetBrowseNeoApiResponse>(
+    `${envs.API_URL}/browse?api_key=${envs.API_KEY}`
+  );
 }
--- src/pages/DataDisplayPage.tsx
+++ src/pages/DataDisplayPage.tsx
@@ -4,37 +4,49 @@ import BarChart from "../components/charts/BarChart";
 import useQuery from "../hooks/useQuery";
 
 export default function DataDisplayPage() {
-  // const { data, error, isLoading } = useQuery({
-  //   queryFn: neoApi.getBrowseNeoApi,
-  //   queryKey: "getBrowseNeoApi",
-  // });
+  const { data, error, isLoading } = useQuery({
+    queryFn: neoApi.getBrowseNeoApi,
+    queryKey: "getBrowseNeoApi",
+    transformFn: (data) => {
+      return data
+        ? data.near_earth_objects.map(({ name, estimated_diameter }) => {
+            return [
+              name,
+              estimated_diameter.kilometers.estimated_diameter_min,
+              estimated_diameter.kilometers.estimated_diameter_max,
+            ];
+          })
+        : [];
+    },
+  });
+  if (isLoading) return <div>Loading</div>;
+  if (error) return <div>{error}</div>;
   return (
     <div>
-      <BarChart
-        titles={[
-          "NEO Name",
-          "Min Estimated Diameter (km)",
-          "Max Estimated Diameter (km)",
-        ]}
-        width="100%"
-        height="400px"
-        data={[
-          [0, 1, 2],
-          [2, 3, 4],
-          [4, 5, 6],
-          [6, 7, 8],
-        ]}
-        options={{
-          legend: { position: "top" },
-          hAxis: {
-            title: "Min Estimated Diameter (km)",
-            minValue: 0,
-          },
-          vAxis: {
-            title: "Neo Name",
-          },
-        }}
-      />
+      {data ? (
+        <BarChart
+          titles={[
+            "NEO Name",
+            "Min Estimated Diameter (km)",
+            "Max Estimated Diameter (km)",
+          ]}
+          width="100%"
+          height="400px"
+          data={data as (string | number)[][]}
+          options={{
+            legend: { position: "top" },
+            hAxis: {
+              title: "Min Estimated Diameter (km)",
+              minValue: 0,
+            },
+            vAxis: {
+              title: "Neo Name",
+            },
+          }}
+        />
+      ) : (
+        <div>No Data to show</div>
+      )}
     </div>
   );
 }
